35cbe381119a6c1ff5f7febce416584436f30a51
luna <lunap@tuta.io>
2026-06-14 00:39:27 +0100

    feat: testing mode, drone, tank

---
+++ src/main/kotlin/net/aechronis/aechronis/constants/Drones.kt
@@ -0,0 +1,59 @@
+package net.aechronis.aechronis.constants
+
+import net.aechronis.combat.objects.Drone
+import net.aechronis.combat.objects.Hitbox
+import net.aechronis.combat.objects.HitboxPart
+import net.kyori.adventure.text.Component
+import net.minestom.server.coordinate.Vec
+
+object Drones {
+    val droneHitbox =
+        Hitbox(
+            listOf(
+                HitboxPart(
+                    offset = Vec(0.0, 0.0, -2.0),
+                    size = Vec(1.0, 1.0, 8.0),
+                    name = "body",
+                ),
+                HitboxPart(
+                    offset = Vec(0.0, -0.5, 0.0),
+                    size = Vec(8.0, 0.5, 2.0),
+                    name = "wing",
+                ),
+            ),
+        )
+
+    val scoutDrone =
+        Drone(
+            name = "drone",
+            itemName = Component.text("drone"),
+            scale = 1.5,
+            hitbox = droneHitbox,
+            maxSpeed = 2F,
+            turnSpeed = 5F,
+            pitchSpeed = 5F,
+            maxRange = 500,
+            batteryLifeTicks = 2000,
+            buzzPeriodTicks = 5,
+        )
+
+    val kamikazeDrone =
+        Drone(
+            name = "drone",
+            itemName = Component.text("drone"),
+            scale = 1.5,
+            hitbox = droneHitbox,
+            maxSpeed = 1F,
+            turnSpeed = 5F,
+            pitchSpeed = 5F,
+            maxRange = 300,
+            batteryLifeTicks = 1000,
+            projectileModel = "aechronis:rpg-rocket",
+            projectileMountOffset = Vec(0.0, 0.5, 0.0),
+            projectileScale = 0.5,
+            explosionRadius = 1,
+            explosionFire = 0.0,
+            explosionDamage = 20F,
+            buzzPeriodTicks = 5,
+        )
+}
---
+++ src/main/kotlin/net/aechronis/aechronis/constants/Tanks.kt
@@ -0,0 +1,32 @@
+package net.aechronis.aechronis.constants
+
+import net.aechronis.combat.objects.Hitbox
+import net.aechronis.combat.objects.HitboxPart
+import net.aechronis.combat.objects.Tank
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
+import net.minestom.server.coordinate.Vec
+
+object Tanks {
+    val m1a1AbramsHitbox =
+        Hitbox(
+            listOf(
+                HitboxPart(
+                    offset = Vec(0.0, 0.0, 0.0),
+                    size = Vec(1.6, 1.5, 2.6),
+                    name = "body",
+                ),
+            ),
+        )
+
+    val m1a1Abrams =
+        Tank(
+            name = "m1a1-abrams",
+            itemName = Component.text("M1A1 Abrams", NamedTextColor.GOLD).decoration(TextDecoration.ITALIC, false),
+            model = "aechronis:m1a1-abrams",
+            hitbox = m1a1AbramsHitbox,
+            scale = 3.0,
+            seatOffsets = listOf(Vec(0.0, 1.0, 0.0)),
+        )
+}
