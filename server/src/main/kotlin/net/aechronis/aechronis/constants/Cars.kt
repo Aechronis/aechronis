7abfe9046190d59750d59cf18101137d31f8067b
luna <lunap@tuta.io>
2026-07-25 16:30:39 +0100

    refactor: add speeds to truck

--- server/src/main/kotlin/net/aechronis/aechronis/constants/Cars.kt
+++ server/src/main/kotlin/net/aechronis/aechronis/constants/Cars.kt
@@ -27,6 +27,8 @@ object Cars {
             health = 500F,
             placeTime = 2000,
             scale = 3.0,
+            maxSpeed = 25f,
+            turnSpeed = 20f,
             maxClimbHeight = 1.5f,
             seatOffsets =
                 listOf(
