a4736f2a360c8580d35c38bbac79532ee94dd1ee
luna <lunap@tuta.io>
2026-07-26 09:26:45 +0100

    refactor: remove view distance setter, we will set this with the server flag

--- server/src/main/kotlin/net/aechronis/aechronis/listeners/PlayerJoinListener.kt
+++ server/src/main/kotlin/net/aechronis/aechronis/listeners/PlayerJoinListener.kt
@@ -8,9 +8,6 @@ import net.kyori.adventure.text.Component
 import net.minestom.server.coordinate.Pos
 import net.minestom.server.entity.GameMode
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
-import net.minestom.server.event.player.PlayerSpawnEvent
-import net.minestom.server.network.packet.server.play.UpdateSimulationDistancePacket
-import net.minestom.server.network.packet.server.play.UpdateViewDistancePacket
 import java.net.URI
 
 object PlayerJoinListener {
@@ -41,15 +38,7 @@ object PlayerJoinListener {
         player.sendResourcePacks(resourcePackRequest)
     }
 
-    private fun onPlayerJoin(event: PlayerSpawnEvent) {
-        val player = event.player
-
-        player.sendPacket(UpdateViewDistancePacket(Aechronis.VIEW_DISTANCE))
-        player.sendPacket(UpdateSimulationDistancePacket(Aechronis.VIEW_DISTANCE))
-    }
-
     fun init() {
         Aechronis.eventNode.addListener(AsyncPlayerConfigurationEvent::class.java, PlayerJoinListener::onAsyncPlayerConfiguration)
-        Aechronis.eventNode.addListener(PlayerSpawnEvent::class.java, PlayerJoinListener::onPlayerJoin)
     }
 }
