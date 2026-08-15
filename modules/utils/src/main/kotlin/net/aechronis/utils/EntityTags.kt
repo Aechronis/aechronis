a5deea134ae24a663cc2b5b945c6aff264b3f0dd
luna <lunap@tuta.io>
2026-08-15 18:21:08 +0100

    fix: commit utils EntityTags

---
+++ modules/utils/src/main/kotlin/net/aechronis/utils/EntityTags.kt
@@ -0,0 +1,9 @@
+package net.aechronis.utils
+
+import net.minestom.server.tag.Tag
+
+object EntityTags {
+    val TRANSIENT_ENTITY: Tag<Boolean> = Tag.Boolean("aechronis:transient_entity")
+
+    val DAMAGEABLE_MANNEQUIN: Tag<Boolean> = Tag.Boolean("aechronis:damageable_mannequin")
+}
