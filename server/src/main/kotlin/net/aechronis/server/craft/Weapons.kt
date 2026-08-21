62b695c3c79e02948dbb0dc57e78538746f6dba0
error11O <error11o@protonmail.com>
2026-08-21 04:09:05 +0100

    feat: basic weapon/armor configs (to be changed tmr if needed)

--- server/src/main/kotlin/net/aechronis/server/craft/Weapons.kt
+++ server/src/main/kotlin/net/aechronis/server/craft/Weapons.kt
@@ -1,8 +1,169 @@
 package net.aechronis.server.craft
 
+import net.aechronis.combat.objects.Hat
+import net.aechronis.combat.storage.HatCollection
+import net.aechronis.server.constants.Ammo
+import net.aechronis.server.constants.Armor
+import net.aechronis.server.constants.Guns
+import net.aechronis.server.constants.Hats
 import net.aechronis.vanilla.objects.Recipe
+import net.aechronis.vanilla.objects.RecipesGrid
+import net.aechronis.vanilla.objects.RecipesIngredient
+import net.aechronis.vanilla.objects.RecipesResult
+import net.aechronis.vanilla.objects.RecipesShapeless
+import net.minestom.server.entity.Player
+import net.minestom.server.item.ItemStack
+import net.minestom.server.item.Material
 
+/** Crafting recipes for player weapons, ammunition, uniforms, and the gas-mask cosmetic. */
 object Weapons {
     val list: List<Recipe> =
-        listOf()
+        buildList {
+            add(
+                shapeless(
+                    ItemStack.of(Material.GUNPOWDER, 6),
+                    Material.COAL to 1,
+                    Material.SULFUR to 2,
+                    Material.BLAZE_POWDER to 2,
+                ),
+            )
+
+            add(shapeless(Ammo.ammo762x39mm.toItemStack().withAmount(4), Material.COPPER_INGOT to 3, Material.GUNPOWDER to 2))
+            add(shapeless(Ammo.ammo9mm.toItemStack().withAmount(4), Material.COPPER_INGOT to 2, Material.GUNPOWDER to 1))
+            add(
+                shapeless(
+                    Ammo.ammo762x39mmExplosive.toItemStack().withAmount(4),
+                    Material.COPPER_INGOT to 3,
+                    Material.GUNPOWDER to 4,
+                ),
+            )
+            add(
+                shapeless(
+                    Ammo.rocket.toItemStack().withAmount(4),
+                    Material.COPPER_INGOT to 3,
+                    Material.IRON_BLOCK to 1,
+                    Material.GUNPOWDER to 5,
+                ),
+            )
+            add(
+                shapeless(
+                    Ammo.tankShell.toItemStack().withAmount(4),
+                    Material.COPPER_INGOT to 4,
+                    Material.IRON_BLOCK to 1,
+                    Material.GUNPOWDER to 4,
+                ),
+            )
+
+            // Uniform recipes use dyes to choose their faction's equipment asset.
+            add(
+                hatUnlockRecipe(
+                    Hats.gasMask,
+                    Material.GOLD_INGOT to 1,
+                    Material.DIAMOND to 1,
+                    Material.IRON_INGOT to 3,
+                    Material.BLACK_DYE to 1,
+                ),
+            )
+            add(
+                shapeless(
+                    Armor.usMarineJacket.toItemStack(),
+                    Material.GOLD_INGOT to 2,
+                    Material.DIAMOND to 2,
+                    Material.IRON_BLOCK to 1,
+                    Material.YELLOW_DYE to 1,
+                ),
+            )
+            add(
+                shapeless(
+                    Armor.usMarineTrousers.toItemStack(),
+                    Material.GOLD_INGOT to 2,
+                    Material.DIAMOND to 2,
+                    Material.IRON_INGOT to 4,
+                    Material.YELLOW_DYE to 1,
+                ),
+            )
+            add(
+                shapeless(
+                    Armor.usMarineBoots.toItemStack(),
+                    Material.GOLD_INGOT to 1,
+                    Material.DIAMOND to 1,
+                    Material.IRON_INGOT to 2,
+                    Material.YELLOW_DYE to 1,
+                ),
+            )
+            add(
+                shapeless(
+                    Armor.idfJacket.toItemStack(),
+                    Material.GOLD_INGOT to 2,
+                    Material.DIAMOND to 2,
+                    Material.IRON_BLOCK to 1,
+                    Material.GREEN_DYE to 1,
+                ),
+            )
+            add(
+                shapeless(
+                    Armor.idfTrousers.toItemStack(),
+                    Material.GOLD_INGOT to 2,
+                    Material.DIAMOND to 2,
+                    Material.IRON_INGOT to 4,
+                    Material.GREEN_DYE to 1,
+                ),
+            )
+            add(
+                shapeless(
+                    Armor.idfBoots.toItemStack(),
+                    Material.GOLD_INGOT to 1,
+                    Material.DIAMOND to 1,
+                    Material.IRON_INGOT to 2,
+                    Material.GREEN_DYE to 1,
+                ),
+            )
+
+            add(shapeless(Guns.m4a1.toEmptyItemStack(), Material.GOLD_BLOCK to 1, Material.DIAMOND to 7, Material.IRON_BLOCK to 1))
+            add(shapeless(Guns.ak12.toEmptyItemStack(), Material.GOLD_BLOCK to 1, Material.DIAMOND to 4, Material.IRON_BLOCK to 1))
+            add(shapeless(Guns.qbz95.toEmptyItemStack(), Material.GOLD_BLOCK to 1, Material.DIAMOND to 5, Material.IRON_BLOCK to 1))
+            add(shapeless(Guns.ak74.toEmptyItemStack(), Material.GOLD_BLOCK to 1, Material.DIAMOND to 3, Material.IRON_BLOCK to 1))
+            add(shapeless(Guns.g3.toEmptyItemStack(), Material.GOLD_BLOCK to 1, Material.DIAMOND to 2, Material.IRON_BLOCK to 1))
+            add(shapeless(Guns.glock17.toEmptyItemStack(), Material.GOLD_BLOCK to 1, Material.DIAMOND to 1, Material.IRON_BLOCK to 1))
+            add(shapeless(Guns.m9.toEmptyItemStack(), Material.GOLD_BLOCK to 1, Material.DIAMOND to 1, Material.IRON_BLOCK to 1))
+            add(shapeless(Guns.mp5.toEmptyItemStack(), Material.GOLD_BLOCK to 1, Material.DIAMOND to 2, Material.IRON_BLOCK to 1))
+            add(shapeless(Guns.vz61.toEmptyItemStack(), Material.GOLD_BLOCK to 1, Material.DIAMOND to 1, Material.IRON_BLOCK to 1))
+            add(shapeless(Guns.mg3.toEmptyItemStack(), Material.GOLD_BLOCK to 1, Material.DIAMOND to 6, Material.IRON_BLOCK to 1))
+            add(shapeless(Guns.at4.toEmptyItemStack(), Material.GOLD_BLOCK to 2, Material.DIAMOND_BLOCK to 1, Material.IRON_BLOCK to 2))
+        }
+
+    private fun shapeless(
+        output: ItemStack,
+        vararg materialCounts: Pair<Material, Int>,
+    ): RecipesShapeless = RecipesShapeless(ingredients(*materialCounts), output)
+
+    private fun hatUnlockRecipe(
+        hat: Hat,
+        vararg materialCounts: Pair<Material, Int>,
+    ): Recipe = HatUnlockRecipe(hat, ingredients(*materialCounts))
+
+    private fun ingredients(vararg materialCounts: Pair<Material, Int>): List<RecipesIngredient> =
+        buildList {
+            for ((material, count) in materialCounts) {
+                require(count > 0) { "Recipe ingredient count must be positive" }
+                repeat(count) { add(RecipesIngredient.of(material)!!) }
+            }
+        }
+
+    private class HatUnlockRecipe(
+        private val hat: Hat,
+        ingredients: List<RecipesIngredient>,
+    ) : Recipe {
+        private val delegate = RecipesShapeless(ingredients, hat.toItemStack())
+
+        override fun match(recipesGrid: RecipesGrid): RecipesResult? = delegate.match(recipesGrid)?.copy(recipe = this)
+
+        override fun canCraft(player: Player): Boolean = !HatCollection.owns(player.uuid, hat)
+
+        override fun grantsItem(): Boolean = false
+
+        override fun onCraft(player: Player) {
+            HatCollection.give(player.uuid, hat)
+        }
+    }
 }
---
+++ server/src/test/kotlin/net/aechronis/server/craft/WeaponsTest.kt
@@ -0,0 +1,153 @@
+package net.aechronis.server.craft
+
+import net.aechronis.combat.constants.Tags
+import net.aechronis.server.constants.Ammo
+import net.aechronis.server.constants.Guns
+import net.aechronis.vanilla.objects.RecipesGrid
+import net.aechronis.vanilla.objects.RecipesGridSlot
+import net.aechronis.vanilla.objects.RecipesShapeless
+import net.minestom.server.MinecraftServer
+import net.minestom.server.component.DataComponents
+import net.minestom.server.item.ItemStack
+import net.minestom.server.item.Material
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+class WeaponsTest {
+    @BeforeTest
+    fun initializeMinecraft() {
+        synchronized(WeaponsTest::class) {
+            if (!minecraftInitialized) {
+                MinecraftServer.init()
+                minecraftInitialized = true
+            }
+        }
+    }
+
+    @Test
+    fun `gunpowder recipe uses five unstacked inputs and yields six gunpowder`() {
+        val result =
+            craft(
+                Material.COAL,
+                Material.SULFUR,
+                Material.SULFUR,
+                Material.BLAZE_POWDER,
+                Material.BLAZE_POWDER,
+            )
+
+        assertEquals(Material.GUNPOWDER, result?.material())
+        assertEquals(6, result?.amount())
+    }
+
+    @Test
+    fun `every ammunition recipe yields four reload items`() {
+        val recipes =
+            listOf(
+                arrayOf(Material.COPPER_INGOT, Material.COPPER_INGOT, Material.COPPER_INGOT, Material.GUNPOWDER, Material.GUNPOWDER),
+                arrayOf(Material.COPPER_INGOT, Material.COPPER_INGOT, Material.GUNPOWDER),
+                arrayOf(
+                    Material.COPPER_INGOT,
+                    Material.COPPER_INGOT,
+                    Material.COPPER_INGOT,
+                    Material.GUNPOWDER,
+                    Material.GUNPOWDER,
+                    Material.GUNPOWDER,
+                    Material.GUNPOWDER,
+                ),
+                arrayOf(
+                    Material.COPPER_INGOT,
+                    Material.COPPER_INGOT,
+                    Material.COPPER_INGOT,
+                    Material.IRON_BLOCK,
+                    Material.GUNPOWDER,
+                    Material.GUNPOWDER,
+                    Material.GUNPOWDER,
+                    Material.GUNPOWDER,
+                    Material.GUNPOWDER,
+                ),
+                arrayOf(
+                    Material.COPPER_INGOT,
+                    Material.COPPER_INGOT,
+                    Material.COPPER_INGOT,
+                    Material.COPPER_INGOT,
+                    Material.IRON_BLOCK,
+                    Material.GUNPOWDER,
+                    Material.GUNPOWDER,
+                    Material.GUNPOWDER,
+                    Material.GUNPOWDER,
+                ),
+            )
+
+        for (inputs in recipes) {
+            assertEquals(4, craft(*inputs)?.amount())
+        }
+    }
+
+    @Test
+    fun `ak12 uses the requested block based cost and starts empty`() {
+        val result =
+            craft(
+                Material.GOLD_BLOCK,
+                Material.DIAMOND,
+                Material.DIAMOND,
+                Material.DIAMOND,
+                Material.DIAMOND,
+                Material.IRON_BLOCK,
+            )
+
+        assertEquals(Guns.ak12.name, result?.getTag(Tags.name))
+        assertEquals(99, result?.get(DataComponents.DAMAGE))
+    }
+
+    @Test
+    fun `no shapeless weapon recipe exceeds the three by three grid`() {
+        assertTrue(
+            Weapons.list
+                .filterIsInstance<RecipesShapeless>()
+                .all { it.recipesIngredients.size <= 9 },
+        )
+    }
+
+    @Test
+    fun `awp has no crafting recipe`() {
+        assertTrue(
+            Weapons.list
+                .filterIsInstance<RecipesShapeless>()
+                .none { it.output.getTag(Tags.name) == Guns.awp.name },
+        )
+    }
+
+    @Test
+    fun `gunpowder is required for normal rifle ammunition`() {
+        assertNull(craft(Material.COPPER_INGOT, Material.COPPER_INGOT, Material.COPPER_INGOT))
+        assertEquals(
+            Ammo.ammo762x39mm.name,
+            craft(
+                Material.COPPER_INGOT,
+                Material.COPPER_INGOT,
+                Material.COPPER_INGOT,
+                Material.GUNPOWDER,
+                Material.GUNPOWDER,
+            )?.getTag(Tags.name),
+        )
+    }
+
+    private fun craft(vararg materials: Material): ItemStack? {
+        val grid =
+            RecipesGrid(
+                3,
+                3,
+                Array(9) { slot ->
+                    RecipesGridSlot(slot, materials.getOrNull(slot)?.let(ItemStack::of) ?: ItemStack.AIR)
+                },
+            )
+        return Weapons.list.firstNotNullOfOrNull { it.match(grid) }?.result
+    }
+
+    private companion object {
+        var minecraftInitialized = false
+    }
+}
