7aba88cf30523946af56f2ff8d6e3886797452a7
error11O <error11o@protonmail.com>
2026-08-25 01:50:18 +0100

    car and baton

--- server/src/main/kotlin/net/aechronis/server/craft/Vehicles.kt
+++ server/src/main/kotlin/net/aechronis/server/craft/Vehicles.kt
@@ -1,8 +1,30 @@
 package net.aechronis.server.craft
 
+import net.aechronis.server.constants.Cars
 import net.aechronis.vanilla.objects.Recipe
+import net.aechronis.vanilla.objects.RecipesIngredient
+import net.aechronis.vanilla.objects.Shaped
+import net.minestom.server.item.Material
 
 object Vehicles {
+    // This is the scout drone recipe with one fewer diamond, making the truck slightly cheaper.
     val list: List<Recipe> =
-        listOf()
+        listOf(
+            Shaped(
+                3,
+                3,
+                arrayOf(
+                    RecipesIngredient.of(Material.DIAMOND)!!,
+                    RecipesIngredient.of(Material.COPPER_INGOT)!!,
+                    RecipesIngredient.of(Material.DIAMOND)!!,
+                    RecipesIngredient.of(Material.REDSTONE)!!,
+                    RecipesIngredient.of(Material.COAL)!!,
+                    RecipesIngredient.of(Material.REDSTONE)!!,
+                    RecipesIngredient.of(Material.DIAMOND)!!,
+                    RecipesIngredient.of(Material.COPPER_INGOT)!!,
+                    null,
+                ),
+                Cars.humvee.toItemStack(),
+            ),
+        )
 }
